69936edada133beb1a7f22437ec42d2329b39480
Weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2026-05-28 03:18:15 +0800

    products: Introduce OnePlus 12R / Ace 3
    Signed-off-by: Weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,4 +7,5 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/X1/X1.mk \
     $(LOCAL_DIR)/fogos/fogos.mk \
     $(LOCAL_DIR)/onyx/onyx.mk \
-    $(LOCAL_DIR)/topaz/topaz.mk
+    $(LOCAL_DIR)/topaz/topaz.mk \
+    $(LOCAL_DIR)/aston/aston.mk
